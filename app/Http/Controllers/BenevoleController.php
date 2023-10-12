09bdcdbe20afc4b4b78b46461bdadbfd68ae1415
achija <r4gamia@gmail.com>
2023-10-12 15:16:20 +0000

    inscription

--- app/Http/Controllers/BenevoleController.php
+++ app/Http/Controllers/BenevoleController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class BenevoleController extends Controller
 {
-    //
+    public function index(){
+        return view('accueil');
+    }
+
+    public function store(Request $request) {
+
+    }
 }

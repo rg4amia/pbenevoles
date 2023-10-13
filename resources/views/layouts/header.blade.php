25bf07efdc732a34553a7b04df3cef8cf4d71eb1
Serge Gouety <sergekrystel@gmail.com>
2023-10-13 08:48:26 +0000

    fix formulaire 13-10-23

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,10 +1,10 @@
 <header>
-    <a id="menu-button-mobile" class="cmn-toggle-switch cmn-toggle-switch__htx" href="javascript:void(0);"><span>Menu mobile</span></a>
+    <!-- <a id="menu-button-mobile" class="cmn-toggle-switch cmn-toggle-switch__htx" href="javascript:void(0);"><span>Menu mobile</span></a>
     <nav class="main_nav">
         <ul class="nav nav-tabs">
             <li><a href="#tab_1" data-bs-toggle="tab" class="active show">Request a quote</a></li>
             <li><a href="#tab_2" data-bs-toggle="tab">About</a></li>
             <li><a href="#tab_3" data-bs-toggle="tab">Contact</a></li>
         </ul>
-    </nav>
+    </nav> -->
 </header><!-- /header -->

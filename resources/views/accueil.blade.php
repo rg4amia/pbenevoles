3dc2535b92038bc388931aca6c9351c35dabee8b
Serge Gouety <sergekrystel@gmail.com>
2023-10-25 12:45:05 +0000

    add numero

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1057,6 +1057,8 @@
                         </div>
                         <div class="col-sm-7">Horaires <br><span style="color:green; font-size: small">Lun-Ven: 07h30 - 16h30</span>
                         </div>
+                        <div class="col-sm-7">Céllulaire <br><span style="color:green; font-size: small">+225-07-02-67-67-67</span>
+                        </div>
                         <div class="col-sm-7">Téléphone <br><span style="color:green; font-size: small">+225-27-22-49-93-04</span>
                         </div>
                         <div class="col-sm-7">Bénévoles inscris <br><span style="color:green; font-size: medium">{{$totalinscris}}</span>
